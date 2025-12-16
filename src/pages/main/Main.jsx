df9768b68320115e2e52c69808b23c8288088d7f
jseunghoon9075-svg <jseunghoon9075@gmail.com>
2025-12-16 16:37:05 +0900

    메인 화면구성svg제거

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -109,10 +109,6 @@ const Main = () => {
 
             <S.SearchButton aria-label="검색">
               {/* 간단한 돋보기 SVG 아이콘 */}
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#fff" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M21 21L16.65 16.65" stroke="#fff" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
             </S.SearchButton>
           </S.SearchWrap>
         </S.MiddleWrap>
